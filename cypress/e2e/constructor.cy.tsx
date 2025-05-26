808ec418eb2df1b949614e61dedc189aabd91bb4
Freezebee <stanislavlav05@gmail.com>
2025-05-26 22:40:27 +0300

    Добавлены константы в тесты

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,16 +1,21 @@
 /// <reference types="cypress" />
 import * as orderFixture from '../fixtures/order.json';
 
+const testUrl = 'http://localhost:4000';
+const [bunSelector, mainSelector, sauceSelector] = ['[data-cy="bun"]', '[data-cy="main"]', '[data-cy="sauce"]'];
+const orderSelector = '[data-cy="order-button"]';
+const modalsSelector = '#modals';
+
 describe('E2E тест', () => {
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
 
-    cy.visit('http://localhost:4000');
+    cy.visit(testUrl);
   });
 
   it('список ингредиентов', () => {
-    cy.get('[data-cy="bun"]').should('have.length.at.least', 1);
-    cy.get('[data-cy="main"],[data-cy="sauce"]').should(
+    cy.get(bunSelector).should('have.length.at.least', 1);
+    cy.get(`${mainSelector},${sauceSelector}`).should(
       'have.length.at.least',
       1
     );
@@ -19,37 +24,37 @@ describe('E2E тест', () => {
   describe('проверка работы модалок описаний ингредиентов', () => {
     describe('проверка открыти модалок', () => {
       it('по карточке', () => {
-        cy.get('[data-cy="bun"]:first-of-type').click();
-        cy.get('#modals').children().should('have.length', 2);
+        cy.get(`${bunSelector}:first-of-type`).click();
+        cy.get(modalsSelector).children().should('have.length', 2);
       });
 
       it('после перезагрузки', () => {
-        cy.get('[data-cy="bun"]:first-of-type').click();
+        cy.get(`${bunSelector}:first-of-type`).click();
         cy.reload(true);
-        cy.get('#modals').children().should('have.length', 2);
+        cy.get(modalsSelector).children().should('have.length', 2);
       });
     });
 
     describe('проверка закрытия модалок', () => {
       it('крестик', () => {
-        cy.get('[data-cy="bun"]:first-of-type').click();
-        cy.get('#modals button:first-of-type').click();
+        cy.get(`${bunSelector}:first-of-type`).click();
+        cy.get(`${modalsSelector} button:first-of-type`).click();
         cy.wait(500);
-        cy.get('#modals').children().should('have.length', 0);
+        cy.get(modalsSelector).children().should('have.length', 0);
       });
 
       it('оверлей', () => {
-        cy.get('[data-cy="bun"]:first-of-type').click();
-        cy.get('#modals>div:nth-of-type(2)').click({ force: true });
+        cy.get(`${bunSelector}:first-of-type`).click();
+        cy.get(`${modalsSelector}>div:nth-of-type(2)`).click({ force: true });
         cy.wait(500);
-        cy.get('#modals').children().should('have.length', 0);
+        cy.get(modalsSelector).children().should('have.length', 0);
       });
 
       it('esc', () => {
-        cy.get('[data-cy="bun"]:first-of-type').click();
+        cy.get(`${bunSelector}:first-of-type`).click();
         cy.get('body').type('{esc}');
         cy.wait(500);
-        cy.get('#modals').children().should('have.length', 0);
+        cy.get(modalsSelector).children().should('have.length', 0);
       });
     });
   });
@@ -63,23 +68,23 @@ describe('E2E тест', () => {
       cy.intercept('POST', 'api/orders', { fixture: 'order.json' });
       cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
 
-      cy.visit('http://localhost:4000');
+      cy.visit(testUrl);
     });
 
     it('оформление заказа', () => {
-      cy.get('[data-cy="order-button"]').should('be.disabled');
-      cy.get('[data-cy="bun"]:first-of-type button').click();
-      cy.get('[data-cy="order-button"]').should('be.disabled');
-      cy.get('[data-cy="main"]:first-of-type button').click();
-      cy.get('[data-cy="order-button"]').should('be.enabled');
+      cy.get(orderSelector).should('be.disabled');
+      cy.get(`${bunSelector}:first-of-type button`).click();
+      cy.get(orderSelector).should('be.disabled');
+      cy.get(`${mainSelector}:first-of-type button`).click();
+      cy.get(orderSelector).should('be.enabled');
 
-      cy.get('[data-cy="order-button"]').click();
-      cy.get('#modals').children().should('have.length', 2);
-      cy.get('#modals h2:first-of-type').should(
+      cy.get(orderSelector).click();
+      cy.get(modalsSelector).children().should('have.length', 2);
+      cy.get(`${modalsSelector} h2:first-of-type`).should(
         'have.text',
         orderFixture.order.number
       );
-      cy.get('[data-cy="order-button"]').should('be.disabled');
+      cy.get(orderSelector).should('be.disabled');
     });
 
     afterEach(() => {
